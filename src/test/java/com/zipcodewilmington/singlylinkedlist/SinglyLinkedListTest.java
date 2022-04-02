551610412b9c9e807b8a86d7c0f3f0d37f296952
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-04-02 16:54:08 -0400

    Pt 1

--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,51 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.testng.annotations.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void copy() {
+    }
+
+    @Test
+    void sort() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void getHeadNode() {
+    }
+
+
 }
