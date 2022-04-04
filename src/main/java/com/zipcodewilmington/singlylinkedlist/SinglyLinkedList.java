da6825488aa4807ae315d398c95889f17405b834
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-04-03 23:24:19 -0400

    Delete at index

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -6,8 +6,13 @@ package com.zipcodewilmington.singlylinkedlist;
 public class SinglyLinkedList<T> {
     public Node headNode;
     public Node tail;
+    public static int size = 1;
 
+    public SinglyLinkedList(){
+        headNode = new Node(null);
+        tail = headNode;
 
+    }
     public SinglyLinkedList(T element) {
         this.headNode = new Node(element);
         tail = headNode;
@@ -17,17 +22,42 @@ public class SinglyLinkedList<T> {
     // add -- add an element to the list
 
     public void add(T element) {
+        size++;
         tail.setNextNode(new Node(element));
         tail = tail.next;
+
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index > size - 1) return;
+
+        if(headNode.index == index){
+            headNode = headNode.next;
+            return;
+        }
+
+        Node currentNode = headNode;
+        Node prevNode = headNode;
+        SinglyLinkedList newList = new SinglyLinkedList();
+
+        for (int i = 0; i < size; i++){
+            if(currentNode.index != index){
+                newList.tail.setNextNode((currentNode));
+                currentNode = currentNode.getNextNode();
+            } else {
+                prevNode.next = currentNode.getNextNode();}
+        }
     }
 
     public static class Node<T> {
 
         public T data;
+        int index;
         private Node next;
 
         public Node(T data) {
             this.data = data;
+            this.index = size - 1;
             this.next = null;
         }
 
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,6 +1,9 @@
 package com.zipcodewilmington.singlylinkedlist;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SinglyLinkedListTest {
 
 //    @Test
@@ -23,10 +26,32 @@ class SinglyLinkedListTest {
     }
 
     @Test
-    void getTail() {
+    void removeTest() {
+        SinglyLinkedList testList = new SinglyLinkedList("Monopoly");
+        testList.add("Ludo");
+        testList.remove(1);
+
+        SinglyLinkedList.Node expected = testList.headNode;
+        SinglyLinkedList.Node actual = testList.tail;
+
+       assertEquals(expected, actual);
+
+
+
+
+        SinglyLinkedList.Node currentNode = testList.headNode;
+
+
+
+
+//        do {System.out.println(currentNode.data);
+//            currentNode =  currentNode.getNextNode();
+//            System.out.println(currentNode.data);
+//        } while(currentNode.getNextNode() != null);
+
     }
 
-    @Test
-    void setTail() {
-    }
+
+
+
 }
